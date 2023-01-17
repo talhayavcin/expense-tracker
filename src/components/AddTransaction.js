05cd90bd8ae6f610b8e644a838f642fca88dbb4c
talhayavcin <talhayavcin35@gmail.com>
2023-01-17 23:52:19 +0300

    finished the application and learned context api but ui design should be reviewed

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
-  const [text, setSext] = useState("");
+  const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 10000000),
+      text,
+      amount: +amount,
+    };
+
+    addTransaction(newTransaction);
+  };
 
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
             type="text"
             value={text}
-            onChange={(e) => setSext(e.target.value)}
+            onChange={(e) => setText(e.target.value)}
             placeholder="Enter text..."
           />
         </div>
